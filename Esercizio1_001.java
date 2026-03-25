257c1bdcc2938b9f30250c4af02e17f7b02410d7
bryanferrari05 <bferrari4@studenti.uninsubria.it>
2026-03-25 11:57:40 +0100

    revisione di due esercizi

--- Esercizio1_001.java
+++ Esercizio1_001.java
@@ -1,14 +1,29 @@
-public class Esercizio1_001 extends Thread {
-    public void run(){
+/*
+    parte il thread principale main
+    parte il secondo thread t1
+    entrambi stampano cose in parallelo
+*/
+
+public class Esercizio1_001 extends Thread { // creazione di una classe che estende thread
+    public void run(){ // codice che esegue il thread parte quando chiamo start()
         for(int i = 0; i < 3; i++){
             System.out.println("Thread");
         }
     }
-    public static void main(String args[]){
-        Esercizio1_001 t1 =  new Esercizio1_001();
-        t1.start();
+    public static void main(String args[]){ // programma prinicpale thread main
+        Esercizio1_001 t1 =  new Esercizio1_001(); // creazione dell'oggetto thread 
+        t1.start(); // avvio del thread 
         for (int i = 0; i < 3; i++) {
             System.out.println("Main");
         }
     }   
 }
+
+
+/*
+- extends Thread → creo un thread
+- run() → cosa fa
+- start() → lo avvia davvero
+- main e t1 → lavorano in parallelo
+- output → NON prevedibile
+*/
--- Esercizio2_001.java
+++ Esercizio2_001.java
@@ -1,17 +1,34 @@
-public class Esercizio2_001 implements Runnable{
-    public void run(){
+/*
+    Stesso concetto di prima ma fatto in un altro modo 
+    anche qui si hanno due thread ma la differenza è che non estendo thread ma uso Runnable
+*/
+
+public class Esercizio2_001 implements Runnable{ // questa classe descrive cosa deve fare un thread (sto creando un task non un thread)
+    public void run(){ // codice da eseguire del thread
         for (int i = 0; i < 3; i++) {
             System.out.println("thread");
         }
     }
 
-    public static void main (String args[]){
-        Esercizio2_001 re = new Esercizio2_001();
-        Thread t1 = new Thread(re);
-        t1.start();
+    public static void main (String args[]){ // thread main
+        Esercizio2_001 re = new Esercizio2_001(); // creazione di un oggetto che implementa runnable
+        Thread t1 = new Thread(re); // creazione di un thread vero e gli passi dentro (re) "questo thread eseguirà il run() di re"
+        t1.start(); // avvio thread
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Main");
         }
     }
 }
+
+/*
+perché può essere meglio runnable?
+- Puoi estendere altre classi (Java non supporta l'ereditarietà multipla)
+- separi la logica (runnable) e l'esecuzione (thread)
+
+👉 Il programma fa:
+
+creo un lavoro (Runnable)
+creo un thread che lo esegue
+partono insieme → output misto
+*/
